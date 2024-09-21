2a1a6fc3b71835e75129ccf18417ac137702450b
Yoshi-233 <957510530@qq.com>
2024-09-21 13:49:05 +0800

    add spot light

---
+++ 7.spotLight/assets/shaders/phong.frag
@@ -0,0 +1,72 @@
+#version 410 core
+out vec4 fragColor;
+
+uniform sampler2D sampler;
+uniform sampler2D specularMaskSampler;
+in vec2 uv;
+in vec3 normal;
+in vec3 worldPosition;
+
+// 光源參數
+uniform vec3 lightPosition;
+uniform vec3 targetDirection;
+
+// 两个角度的cos值在cpu端计算
+uniform float innerLine;
+uniform float outerLine;
+
+uniform vec3 lightColor;
+uniform vec3 ambientColor;//环境光
+
+// 相机世界位置
+uniform vec3 cameraPosition;
+
+uniform float specularIntensity;
+
+uniform float shiness;
+
+void main()
+{
+        // 1. 计算光照的通用数据
+        vec3 objColor = texture(sampler, uv).rgb;//物体颜色
+        // diffuse 数据
+        vec3 normalN = normalize(normal);
+        vec3 lightDirN = normalize(worldPosition - lightPosition);
+        // 摄像机到物体向量 数据
+        vec3 viewDir = normalize(worldPosition - cameraPosition);
+        vec3 targetDirN = normalize(targetDirection);
+
+        // 计算spotLight的照射范围
+        // float cTheta = dot(lightDirN, targetDirN);
+        // float cVisiable = cos(innerAngle); // glsl这里传入的是弧度
+        // float spotFlag = step(cVisiable, cTheta); // 只有cTheta > cVisiable时才返回1，否则返回0
+        float cGamma = dot(lightDirN, targetDirN);
+        float spotIntensity = clamp((cGamma - outerLine) / (innerLine - outerLine), 0.0f, 1.0f);
+
+        // 2. 准备漫反射相关数据
+        float diffuse = clamp(dot(-lightDirN, normalN), 0.0f, 1.0f);// cos夹角
+        vec3 diffuseColor = lightColor * diffuse * objColor;
+
+        // 3. 计算specular
+        // 防止背面光的照入
+        float dotResult = dot(-lightDirN, normalN);
+        float specularFlag = step(0.0f, dotResult);// 大于0返回1，否则返回0，类似if
+
+        vec3 lightReflect = normalize(reflect(lightDirN, normalN));
+        float specular = max(dot(lightReflect, -viewDir), 0.0f);
+        specular = pow(specular, shiness);
+        // 高光蒙板
+        float specularMask = texture(specularMaskSampler, uv).r;
+
+        // 这里并不需要objColor，光经过物体反射出去了，漫反射是物体吸收
+        // specularIntensity控制光斑呈现的亮度
+        vec3 specularColor = lightColor * specular * specularFlag * specularIntensity * specularMask;
+
+        // 4. 环境光
+        vec3 ambientColor = objColor * ambientColor;
+
+        // 最终计算
+        vec3 finalColor = (diffuseColor + specularColor) * spotIntensity + ambientColor;
+
+        fragColor = vec4(finalColor, 1.0);
+}
---
+++ 7.spotLight/prepare.cpp
@@ -0,0 +1,139 @@
+//
+// Created by nio on 24-9-18.
+#include "glframework/include/core.h"
+#include "glframework/include/shader.hpp"
+#include "glframework/include/texture.h"
+#include "glframework/include/geometry.h"
+#include "glframework/renderer/include/renderer.h"
+#include "application/app/Application.h"
+#include "common/include/check_err.h"
+#include "common/include/common.h"
+
+#include "application/camera/include/camera.h"
+#include "application/camera/include/orthographicCamera.h"
+#include "application/camera/include/perspectiveCamera.h"
+#include "application/camera/include/trackballCameraController.h"
+#include "application/camera/include/gameCameraController.h"
+
+#include "glframework/materials/include/phongMaterial.h"
+#include "glframework/materials/include/whiteMaterial.h"
+#include "glframework/include/mesh.h"
+
+glm::mat4 transform{1.0f};
+
+std::unique_ptr<Renderer> renderer;
+std::shared_ptr<Mesh> whiteMesh;
+std::vector<std::shared_ptr<Mesh>> meshes{};
+std::shared_ptr<PointLight> pointLight;
+std::shared_ptr<SpotLight> spotLight;
+std::shared_ptr<AmbientLight> ambLight;
+
+std::shared_ptr<Camera> camera;
+std::shared_ptr<CameraControl> cameraControl;
+
+
+static void prepareCamera()
+{
+        camera = std::make_shared<PerspectiveCamera>(60.0f,
+                                                     (float) APP.getWidth() / (float) APP.getHeight(),
+                                                     0.1f, 1000.f);
+
+        cameraControl = std::make_shared<GameCameraController>();
+        cameraControl->setCamera(camera.get());
+        // 智能指针安全向下转型
+        std::dynamic_pointer_cast<GameCameraController>(cameraControl)->setSpeed(0.4);
+}
+
+
+void prepareAll()
+{
+        renderer = std::make_unique<Renderer>();
+
+        /* 1.创建geometry */
+        auto geometry = Geometry::createBox(2.5f);
+
+        /* 2. 创建material配置参数 */
+        auto material = std::make_shared<PhongMaterial>();
+        material->setShiness(16.0);
+        material->setDiffuse(std::make_shared<Texture>("assets/textures/box.png", 0));
+        material->setSpecularMask(std::make_shared<Texture>("assets/textures/sp_mask.png", 1));
+
+        /* 3. 生成mesh*/
+        auto mesh = std::make_shared<Mesh>(geometry, material);
+        mesh->setPosition(glm::vec3(0.0f, 0.0f, 0.0f));
+        meshes.push_back(mesh);
+
+        /* 4. 创建light */
+        spotLight = std::make_shared<SpotLight>();
+        ambLight = std::make_shared<AmbientLight>();
+        ambLight->setColor(glm::vec3(0.2f));
+
+        /* 5. 创建点光源 */
+        auto whiteGeometry = Geometry::createSphere(0.1f);
+        auto whiteMaterial = std::make_shared<WhiteMaterial>();
+        whiteMesh = std::make_shared<Mesh>(whiteGeometry, whiteMaterial);
+        whiteMesh->setPosition(glm::vec3(2.0f, 0.0f, 0.0f));
+        spotLight->setPosition(whiteMesh->getPosition());
+        spotLight->setTargetDirection(glm::vec3(-1.0f, 0.0f, 0.0f));
+        spotLight->setInnerAngle(30.0f);
+        spotLight->setOuterAngle(45.0f);
+
+        meshes.push_back(whiteMesh);
+
+        prepareCamera();
+}
+
+// 窗口大小改变回调函数
+static void OnResize(int width, int height)
+{
+        GL_CHECK_ERR(glViewport(0, 0, width, height));
+}
+
+static void OnKeyBoard(int key, int scancode, int action, int mode)
+{
+        cameraControl->onKey(key, action, mode);
+}
+
+static void OnMouse(int button, int action, int mods)
+{
+        auto xPos = std::make_shared<double>();
+        auto yPos = std::make_shared<double>();
+        APP.getCursorPos(xPos.get(), yPos.get());
+
+        cameraControl->onMouse(button, action, *xPos, *yPos);
+}
+
+static void OnCursorPos(double xpos, double ypos)
+{
+        cameraControl->onCursor(xpos, ypos);
+}
+
+static void OnScroll(double yoffset)
+{
+        cameraControl->onScroll(static_cast<float>(yoffset));
+}
+
+void setAPPFunctions()
+{
+        APP.setResizeCallback(OnResize);
+        APP.setKeyCallback(OnKeyBoard);
+        APP.setMouseButtonCallback(OnMouse);
+        APP.setCursorPosCallback(OnCursorPos);
+        APP.setScrollCallback(OnScroll);
+}
+
+void doTransform()
+{
+        auto xPos = static_cast<float>(glm::sin(glfwGetTime()) + 3.0f);
+        whiteMesh->setPosition(glm::vec3(xPos,
+                                         whiteMesh->getPosition().y,
+                                         whiteMesh->getPosition().z));
+        spotLight->setPosition(whiteMesh->getPosition());
+        // transform = glm::rotate(transform, 0.03f, glm::vec3(0.0f, 1.0f, 1.0f));
+}
+
+void render()
+{
+        renderer->render(meshes, camera, spotLight, ambLight);
+}
+
