31ece2c000c06f2805786e58b48dcb56d570994d
yoshi.shao <yoshi.shao@nio.com>
2024-09-27 10:28:39 +0800

    add zfighting

---
+++ 18.DepthZFighting/glframework/materials/include/depthMaterial.h
@@ -0,0 +1,16 @@
+//
+// Created by nio on 24-9-20.
+//
+
+#pragma once
+
+#include "material.h"
+#include "../../include/texture.h"
+
+class DepthMaterial : public Material {
+public:
+        DepthMaterial();
+        ~DepthMaterial();
+private:
+};
+
---
+++ 18.DepthZFighting/glframework/materials/include/material.h
@@ -0,0 +1,39 @@
+//
+// Created by nio on 24-9-19.
+//
+
+#ifndef RENDERER_MATERIAL_H
+#define RENDERER_MATERIAL_H
+
+#include "../../include/core.h"
+
+enum class MaterialType {
+        PhongMaterial,
+        WhiteMaterial,
+        DepthMaterial
+};
+
+class Material {
+public:
+        Material();
+
+        ~Material();
+
+public:
+        MaterialType mType;
+
+public:
+        /* 深度检测相关 */
+        bool mDepthTest{true};
+        GLenum mDepthFunc{GL_LESS};
+        bool mDepthWrite{true};
+
+public:
+        /* PolygonOffset */
+        bool mPolygonOffset{false};
+        unsigned int mPolygonOffsetType{GL_POLYGON_OFFSET_FILL};
+        float mFactor{0.0f};
+        float mUint{0.0f};
+};
+
+#endif //RENDERER_MATERIAL_H
